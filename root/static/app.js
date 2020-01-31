751886df8a5b0bd27671c3baef31d4331f9738c0
cha <cha@hqvm-beta-1.atikon.io>
2020-01-31 10:28:22 +0100

    restructure js by bringing in some OO

--- root/static/app.js
+++ root/static/app.js
@@ -6,18 +6,21 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
+function SaveForm(form) {
 
-    const form = document.getElementById('save_ride_form');
-    if (form) {
+    this.form = form;
 
-        form.addEventListener('submit', function(e) {
+    this.initEvents = function() {
+
+        const that = this;
+
+        that.form.addEventListener('submit', function(e) {
 
             e.preventDefault();
 
             const formData = new FormData(this);
 
-            fetch(form.action, {
+            fetch(that.form.action, {
                 method: 'POST',
                 body: formData
             })
@@ -30,35 +33,117 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 console.error('Error:', error);
             });
         });
-    }
 
-    const rides = document.getElementById('rides');
-    if (rides) {
+        return this;
+    };
+}
 
-        fetch(rides.dataset.uri, {
+function RidesOverview(container, rowTemplate) {
+
+    this.container   = container;
+    this.rowTemplate = rowTemplate
+
+    this.get = function() {
+
+        const that = this;
+
+        fetch(that.container.dataset.uri, {
             method: 'GET'
         })
         .then((response) => response.json())
         .then((json) => {
 
-            var tb = document.querySelector('#rides_table tbody');
-            var t  = document.querySelector('#ride_row');
-            td = t.content.querySelectorAll('td');
-
-            json.results.forEach(function(result) {
-
-                td[0].textContent = result.driver;
-                td[1].textContent = result.start_from;
-                td[2].textContent = result.go_to;
-                td[3].textContent = result.start_dt;
-                td[4].textContent = result.seats;
-
-                var clone = document.importNode(t.content, true);
-                tb.appendChild(clone);
-            });
+            that.render(json);
         })
         .catch((error) => {
             console.error('Error:', error);
         });
+
+        return this;
+    };
+
+    this.render = function(json) {
+
+        const that = this;
+
+        var tb = this.container.querySelector('tbody');
+
+        td = this.rowTemplate.content.querySelectorAll('td');
+
+        json.results.forEach(function(result) {
+
+            td[0].textContent = result.driver;
+            td[1].textContent = result.start_from;
+            td[2].textContent = result.go_to;
+            td[3].textContent = result.start_dt;
+            td[4].textContent = result.seats;
+
+            var clone = document.importNode(that.rowTemplate.content, true);
+            tb.appendChild(clone);
+        });
+
+        return this;
+    }
+
+    this.clear = function() {
+
+        [].forEach.call(this.container.querySelectorAll('tbody tr'), function(row) {
+
+            row.parentNode.removeChild(row);
+        });
+
+        return this;
+    }
+}
+
+function Quicksearch(input, ridesOverview) {
+
+    this.input         = input;
+    this.ridesOverview = ridesOverview;
+
+    this.initEvents = function() {
+
+        const that = this;
+
+        that.input.addEventListener('keyup', function(e) {
+
+            fetch(that.input.dataset.uri, {
+                method: 'GET'
+            })
+            .then((response) => response.json())
+            .then((json) => {
+
+                that.ridesOverview.clear().render(json);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        });
+
+        return this;
+    };
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    const form = document.getElementById('save_ride_form');
+    if (form) {
+
+        new SaveForm(form).initEvents();
+    }
+
+    const ridesContainer   = document.getElementById('rides');
+    const ridesRowTemplate = document.getElementById('ride_row');
+    if (ridesContainer) {
+
+        const ridesOverview = new RidesOverview(ridesContainer, ridesRowTemplate);
+
+        ridesOverview.get();
+
+        const quicksearchInput = document.getElementById('quicksearch');
+        if (quicksearchInput) {
+
+            new Quicksearch(quicksearchInput, ridesOverview).initEvents();
+        }
     }
 })
